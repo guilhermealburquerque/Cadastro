f89e82bea6d750e20c5b1c56a7782a199ff632f0
guilhermealburquerque <guilhermeal3325@gmail.com>
2026-06-03 02:21:19 +0000

    tuplas

--- cadastro.py
+++ cadastro.py
@@ -4,3 +4,9 @@
 # ============================================================
  
 # ==================== ESTRUTURAS DE DADOS ====================
+
+# TUPLAS — dados fixos do sistema
+TURNOS = ("Manhã", "Tarde", "Noite")
+SITUACOES = ("Aprovado", "Reprovado", "Em andamento")
+INFO_SISTEMA = ("EduTrack", "v1.0", "2025")
+
